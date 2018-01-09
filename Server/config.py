b61578005b74071c42e27493399b0bc46ea477cf
ndass6 <ND@USComputers.net>
2018-01-09 16:42:21 -0500

    Updated server config

--- Server/config.py
+++ Server/config.py
@@ -1,19 +1,17 @@
 class Config(object):
-    MYSQL_DATABASE_USER = 'sql9194958'
-    MYSQL_DATABASE_PASSWORD = 'lS8bVtrWCv'
-    MYSQL_DATABASE_DB = 'sql9194958'
+    MYSQL_DATABASE_USER = 'sql9214426'
+    MYSQL_DATABASE_PASSWORD = 'EApraVLeK5'
+    MYSQL_DATABASE_DB = 'sql9214426'
     MYSQL_DATABASE_HOST = 'sql9.freemysqlhosting.net'
 
-    AMADEUS_KEY = 'Uaks7cNqc5iA9ECKqotOd4OjRcGz6RDW'
-
-# hackmit2017.pythonanywhere.com
+# ndass1.pythonanywhere.com
 class ProductionConfig(Config):
     ENV = "Prod"
 
-    MYSQL_DATABASE_USER = 'hackmit2017'
-    MYSQL_DATABASE_PASSWORD = 'MITdb123'
-    MYSQL_DATABASE_DB = 'hackmit2017$default'
-    MYSQL_DATABASE_HOST = 'hackmit2017.mysql.pythonanywhere-services.com'
+    MYSQL_DATABASE_USER = 'ndass1'
+    MYSQL_DATABASE_PASSWORD = 'FaceDetectiondb123'
+    MYSQL_DATABASE_DB = 'ndass1$default'
+    MYSQL_DATABASE_HOST = 'ndass1.mysql.pythonanywhere-services.com'
 
 # 127.0.0.1:5000
 class DevelopmentConfig(Config):
