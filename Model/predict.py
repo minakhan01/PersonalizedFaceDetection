be041860d180b807bcecac3f4a2164b6893d7ad1
Mina Khan <minkhn01>
2018-01-30 09:13:04 -0500

    predict for all four classes

--- Model/predict.py
+++ Model/predict.py
@@ -9,9 +9,12 @@ img_width, img_height = 150, 150
 
 model_path = 'model.h5'
 # input_path = 'face_data/train/clinton/0.jpg'      # Should output 0
-input_path = 'face_data/train/obama/0.jpg'          # Should output 1
+input_path1 = 'face_data/train/Barrack Obama/0.jpg'          # Should output 1
+input_path2 = 'face_data/train/Aishwarya Rai/5.jpg'          # Should output 1
+input_path3 = 'face_data/train/Hillary Clinton/0.jpg'          # Should output 1
+input_path4 = 'face_data/train/Michael Jackson/3.jpg'          # Should output 1
 
-def predict():
+def predict(input_path):
     model = load_model(model_path)
 
     img = cv2.imread(input_path)
@@ -24,5 +27,8 @@ def predict():
 
 print("Started program.")
 curr = time.time()
-predict()
+predict(input_path1)
+predict(input_path2)
+predict(input_path3)
+predict(input_path4)
 print("Predicted in " + str(time.time() - curr) + " seconds.")
